1a182223b799ec063ea0716714dc455ea6cefaed
siyovush-hamidov <siyovushchik14@gmail.com>
2024-08-02 00:09:05 +0500

    added src for the future

---
+++ src/level19.sol
@@ -0,0 +1,38 @@
+// You've uncovered an Alien contract. Claim ownership to complete the level.
+
+//   Things that might help
+
+//     Understanding how array storage works
+//     Understanding ABI specifications
+//     Using a very underhanded approach
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.5.0;
+
+import "../helpers/Ownable-05.sol";
+
+contract AlienCodex is Ownable {
+    bool public contact;
+    bytes32[] public codex;
+
+    modifier contacted() {
+        assert(contact);
+        _;
+    }
+
+    function makeContact() public {
+        contact = true;
+    }
+
+    function record(bytes32 _content) public contacted {
+        codex.push(_content);
+    }
+
+    function retract() public contacted {
+        codex.length--;
+    }
+
+    function revise(uint256 i, bytes32 _content) public contacted {
+        codex[i] = _content;
+    }
+}
